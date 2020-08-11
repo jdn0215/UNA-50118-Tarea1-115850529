6e30dadea51ccdc19bb652d401e36e11620f56ab
Daniel Salas <jdn0213@gmail.com>
2020-08-10 22:22:21 -0600

    Funciones basicas completas

--- handlers.go
+++ handlers.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"path"
 )
@@ -29,6 +30,7 @@ func handleGet(w http.ResponseWriter, r *http.Request) (err error) {
 }
 
 func handlePut(w http.ResponseWriter, r *http.Request) (err error) {
+	fmt.Println("recibiendo put")
 	len := r.ContentLength
 	body := make([]byte, len)
 	r.Body.Read(body)
@@ -40,11 +42,35 @@ func handlePut(w http.ResponseWriter, r *http.Request) (err error) {
 }
 
 func handlePost(w http.ResponseWriter, r *http.Request) (err error) {
+	fmt.Println("recibiendo POST")
+	//Buscando el libro segun id
+	id := path.Base(r.URL.Path)
+	checkError("Parse error", err)
+	i := find(id)
+	if i == -1 {
+		return
+	}
+	//Consiguinedo los nuevos valores del libro
+	len := r.ContentLength
+	body := make([]byte, len)
+	r.Body.Read(body)
+	book := Book{}
+	json.Unmarshal(body, &book)
+	//actualizando libro
+	books[i] = book
 	w.WriteHeader(200)
 	return
 }
 
 func handleDelete(w http.ResponseWriter, r *http.Request) (err error) {
+	fmt.Println("recibiendo delete")
+	id := path.Base(r.URL.Path)
+	checkError("Parse error", err)
+	i := find(id)
+	if i == -1 {
+		return
+	}
+	books = append(books[:i], books[i+1:]...)
 	w.WriteHeader(200)
 	return
 }
--- main.go
+++ main.go
@@ -2,18 +2,35 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"net/http"
 )
 
 func handler(writer http.ResponseWriter, request *http.Request) {
-    fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
+	var err error
+	readData("books.csv")
+	switch request.Method {
+	case "GET":
+		fmt.Println("GET....")
+		err = handleGet(writer, request)
+	case "POST":
+		fmt.Println("POST....")
+		err = handlePost(writer, request)
+	case "PUT":
+		fmt.Println("PUT....")
+		err = handlePut(writer, request)
+	case "DELETE":
+		fmt.Println("DELETE....")
+		err = handleDelete(writer, request)
+	}
+	writeData("books.csv")
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func main() {
-	fmt.Println(os.Getenv("PORT"))//-- Resultado daba blanco
-	http.HandleFunc("/", handler)
-	//http.ListenAndServe(":8080", nil) 
-	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-
+	fmt.Println("Servidor iniciado....")
+	http.HandleFunc("/book/", handler)
+	http.ListenAndServe(":8080", nil)
 }
