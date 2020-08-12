6c14c51ccce21866b3acb7a5ba705db9dc90c498
Daniel Salas <jdn0213@gmail.com>
2020-08-11 19:01:37 -0600

    primer ejercicio listo

--- handlers.go
+++ handlers.go
@@ -19,13 +19,23 @@ func find(x string) int {
 func handleGet(w http.ResponseWriter, r *http.Request) (err error) {
 	id := path.Base(r.URL.Path)
 	checkError("Parse error", err)
+	if id == "book" {
+		return handleAll(w, r)
+	}
 	i := find(id)
 	if i == -1 {
 		return
 	}
-	dataJson, err := json.Marshal(books[i])
+	dataJSON, err := json.Marshal(books[i])
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(dataJson)
+	w.Write(dataJSON)
+	return
+}
+
+func handleAll(w http.ResponseWriter, r *http.Request) (err error) {
+	dataJSON, err := json.Marshal(books)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(dataJSON)
 	return
 }
 
